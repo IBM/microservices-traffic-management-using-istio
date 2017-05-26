7b279ea02987ec4154a61c3d28d53775e5487a99
AnthonyAmanse <joe.amanse@obf.ateneo.edu>
2017-05-26 11:46:04 -0700

    Added Travis CI

---
+++ scripts/install.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+function install_bluemix_cli() {
+#statements
+echo "Installing Bluemix cli"
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+sudo mv cf /usr/local/bin
+sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
+cf --version
+curl -L public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_0.5.1_amd64.tar.gz > Bluemix_CLI.tar.gz
+tar -xvf Bluemix_CLI.tar.gz
+sudo ./Bluemix_CLI/install_bluemix_cli
+}
+
+function bluemix_auth() {
+echo "Authenticating with Bluemix"
+echo "1" | bx login -a https://api.ng.bluemix.net -u $BLUEMIX_USER -p $BLUEMIX_PASS
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+bx plugin install container-service -r Bluemix
+echo "Installing kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+}
+
+function cluster_setup() {
+bx cs workers cluster-travis
+$(bx cs cluster-config cluster-travis | grep export)
+
+curl -L https://git.io/getIstio | sh -
+cd $(ls | grep istio)
+export PATH=$PWD/bin:$PATH
+echo "default" | ./samples/apps/bookinfo/cleanup.sh
+
+kubectl apply -f install/kubernetes/istio-rbac-alpha.yaml
+kubectl apply -f install/kubernetes/istio.yaml
+
+PODS=$(kubectl get pods | grep istio | grep Pending)
+while [ ${#PODS} -ne 0 ]
+do
+    echo "Some Pods are Pending..."
+    PODS=$(kubectl get pods | grep istio | grep Pending)
+    sleep 5s
+done
+
+PODS=$(kubectl get pods | grep istio | grep ContainerCreating)
+while [ ${#PODS} -ne 0 ]
+do
+    echo "Some Pods are still creating Containers..."
+    PODS=$(kubectl get pods | grep istio | grep ContainerCreating)
+    sleep 5s
+done
+}
+
+function initial_setup() {
+echo "Creating BookInfo with Injected Envoys..."
+PODS=$(kubectl get pods | grep Init)
+while [ ${#PODS} -ne 0 ]
+do
+    echo "Some Pods are Initializing..."
+    PODS=$(kubectl get pods | grep Init)
+    sleep 5s
+done
+
+echo "BookInfo done."
+
+}
+
+function health_check() {
+
+export GATEWAY_URL=$(kubectl get po -l istio=ingress -o jsonpath={.items[0].status.hostIP}):$(kubectl get svc istio-ingress -o jsonpath={.spec.ports[0].nodePort})
+HEALTH=$(curl -o /dev/null -s -w "%{http_code}\n" http://$GATEWAY_URL/productpage)
+if [ $HEALTH -eq 200 ]
+then
+  echo "Everything looks good."
+  echo "default" | ./samples/apps/bookinfo/cleanup.sh
+else
+  exit 1
+fi
+}
+
+
+
+install_bluemix_cli
+bluemix_auth
+cluster_setup
+initial_setup
+getting_ip_port
+health_check
