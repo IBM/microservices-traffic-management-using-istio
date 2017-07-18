9fcbdca4fb059361c1b163e2754b97649d540cde
Anthony Amanse <joe.amanse@obf.ateneo.edu>
2017-07-18 15:47:58 -0700

    Update travis and toolchain script (#45)
    * Update travis and toolchain script
    
    * Added missing kubectl apply

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,11 +26,15 @@ function cluster_setup() {
 bx cs workers $CLUSTER_NAME
 $(bx cs cluster-config $CLUSTER_NAME | grep export)
 
-sed -i s#PLACEHOLDER_DB_USER#book_user#g $(ls | grep new)
-sed -i s#PLACEHOLDER_DB_PASSWORD#password#g $(ls | grep new)
-sed -i s#PLACEHOLDER_DB_HOST#book-database#g $(ls | grep new)
-sed -i s#PLACEHOLDER_DB_PORT#3306#g $(ls | grep new)
+export USERNAME_BASE64=$(echo -n book_user | base64)
+export PASSWORD_BASE64=$(echo -n password | base64)
+export HOST_BASE64=$(echo -n book-database | base64)
+export PORT_BASE64=$(echo -n 3306 | base64)
 
+sed -i s#"YWRtaW4="#$USERNAME_BASE64#g secrets.yaml
+sed -i s#"VEhYTktMUFFTWE9BQ1JPRA=="#$PASSWORD_BASE64#g secrets.yaml
+sed -i s#"c2wtdXMtc291dGgtMS1wb3J0YWwuMy5kYmxheWVyLmNvbQ=="#$HOST_BASE64#g secrets.yaml
+sed -i s#"MTg0ODE="#$PORT_BASE64#g secrets.yaml
 
 curl -L https://git.io/getIstio | sh -
 cd $(ls | grep istio)
@@ -46,6 +50,7 @@ kubectl delete --ignore-not-found=true -f ../book-database.yaml
 kubectl delete --ignore-not-found=true -f ../details-new.yaml
 kubectl delete --ignore-not-found=true -f ../ratings-new.yaml
 kubectl delete --ignore-not-found=true -f ../reviews-new.yaml
+kubectl delete --ignore-not-found=true -f secrets.yaml
 kuber=$(kubectl get pods | grep Terminating)
 while [ ${#kuber} -ne 0 ]
 do
@@ -77,6 +82,7 @@ echo "Istio setup done."
 
 function initial_setup() {
 echo "Creating BookInfo with Injected Envoys..."
+kubectl apply -f secrets.yaml
 echo "Creating local MySQL database..."
 kubectl apply -f <(istioctl kube-inject -f ../book-database.yaml)
 echo "Creating product page and ingress resource..."
@@ -119,6 +125,7 @@ then
   kubectl delete --ignore-not-found=true -f ../details-new.yaml
   kubectl delete --ignore-not-found=true -f ../ratings-new.yaml
   kubectl delete --ignore-not-found=true -f ../reviews-new.yaml
+  kubectl delete --ignore-not-found=true -f secrets.yaml
   kuber=$(kubectl get pods | grep Terminating)
   while [ ${#kuber} -ne 0 ]
   do
